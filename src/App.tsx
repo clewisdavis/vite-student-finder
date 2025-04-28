2cc0a9c5c80276df16f05a7bcdae6e6742c32f7d
Chris <chrislewis.davis@gmail.com>
2025-04-28 08:23:51 -0400

    refactor: remove unused Button component from App and tidy up Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from '@/components/ui/button';
 import { Header } from '@/components/ui/header';
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Header />
-      <Button>Click me</Button>
     </div>
   );
 }
